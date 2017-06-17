50f7b82c07cfd913e4f1bbaca3b4fc80dc1badd7
Philip <philip.aberneithy@acp.aws.accenture.com>
2017-06-17 18:59:51 +0100

    Redux getState() to get initial application state

--- examples/todos/src/index.js
+++ examples/todos/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react'
-import { render } from 'react-dom'
+// import React from 'react'
+// import { render } from 'react-dom'
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
+// import { Provider } from 'react-redux'
+// import App from './components/App'
+// import reducer from './reducers'
 
-const store = createStore(reducer)
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const counter = (state = 0, action) => {
+
+  switch (action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counter);
+
+console.log('initial application state');
+console.log(store.getState());
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// )
