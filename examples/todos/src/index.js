79a5eeafbf3fcf141143aee8aa57881839b80f71
Philip <philip.aberneithy@acp.aws.accenture.com>
2017-06-18 08:00:24 +0100

    Cleaning up Redux index

--- examples/todos/src/index.js
+++ examples/todos/src/index.js
@@ -1,6 +1,7 @@
-// import React from 'react'
-// import { render } from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+// import { render } from 'react-dom'
 // import { Provider } from 'react-redux'
 // import App from './components/App'
 // import reducer from './reducers'
@@ -18,10 +19,41 @@ const counter = (state = 0, action) => {
   }
 };
 
-const store = createStore(counter);
+let store = createStore(counter);
 
 console.log('initial application state');
+
+//Redux store.getState()
 console.log(store.getState());
+
+//Redux store.dispatch()
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState());
+
+document.addEventListener('click', () => {
+  store.dispatch({ type : 'INCREMENT'});
+});
+
+const Counter = ({value}) => (
+  <h1>{value}</h1>
+);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter value = {store.getState()}/>,
+    document.getElementById('root')
+  )
+};
+
+//Redux subscribe lets you register a callback, that is hit anytime an action is dispatched,
+//so that you can update the UI of your application to reflect the current application state
+store.subscribe(render);
+
+//Initially render
+render();
+
+
+
 
 // render(
 //   <Provider store={store}>
